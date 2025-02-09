fbdcd00ce3eafc655e9f74dcfc3d7230671c94da
Samuel Josephs <samuel.josephs@chicagoclubteam.org>
2025-02-09 21:26:22 +0000

    Fixed indentation in __setitem__

--- aqua_blue/time_series.py
+++ aqua_blue/time_series.py
@@ -56,20 +56,20 @@ class TimeSeries:
         ))
 
     def __setitem__(self,key,value: TimeSeries):
-    if not isinstance(value, TimeSeries):
-        raise TypeError("Value must be a TimeSeries object")
-        
-    if isinstance(key, slice):
-        if key.stop is not None and key.stop > len(self.dependent_variable):
-            raise ValueError("Slice stop index must be within valid range")
-        # Make sure the slice lengths match
-        src_len = len(value.dependent_variable[key])
-        dst_len = len(range(*key.indices(len(self.dependent_variable))))
-        if src_len != dst_len:
-            raise ValueError("Source and destination slices must have the same length")
-        else:
-            if key >= len(self.dependent_variable) or key >= len(self.times):
-                raise ValueError("Key must be less than the length of the time series")
+        if not isinstance(value, TimeSeries):
+            raise TypeError("Value must be a TimeSeries object")
+            
+        if isinstance(key, slice):
+            if key.stop is not None and key.stop > len(self.dependent_variable):
+                raise ValueError("Slice stop index must be within valid range")
+            # Make sure the slice lengths match
+            src_len = len(value.dependent_variable[key])
+            dst_len = len(range(*key.indices(len(self.dependent_variable))))
+            if src_len != dst_len:
+                raise ValueError("Source and destination slices must have the same length")
+            else:
+                if key >= len(self.dependent_variable) or key >= len(self.times):
+                    raise ValueError("Key must be less than the length of the time series")
 
         self.dependent_variable[key] = value.dependent_variable[key] # numpy handles setting slices nicely so no extra explicit logic is needed.
         self.times[key] = value.times[key]
